90d59f07e43871bcfa15cefd7e0e1ce4592c29c2
Simanta Deb Turja <simantaturja@gmail.com>
2021-05-24 12:50:49 +0600

    fixed bug for Issue #5 - sending custom response status exception from catch block - author controller

--- src/main/java/com/example/crudrestapi/controller/AuthorController.java
+++ src/main/java/com/example/crudrestapi/controller/AuthorController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class AuthorController {
@@ -64,7 +65,7 @@ public class AuthorController {
                     new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
             );
         } catch(BadCredentialsException e) {
-            throw new Exception("Incorrect Username or Password ", e);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Incorrect Username or Password ", e);
         }
         final UserDetails userDetails = loginUserService
                 .loadUserByUsername(authenticationRequest.getUsername());
